a74459c202b845ee4c4aaa41d73bba23cca1cfbe
Thomas Osburn <tosburn3@gmail.com>
2020-06-12 23:12:44 -0400

    Took the funcitoning code to import data and modified it to increase performance. Switched order of loops so that each element of the file is read only once. Changed x to y to match the notation used in the existing code. See comments in code for more details.

--- ComplexAmpTest.cc
+++ ComplexAmpTest.cc
@@ -1,144 +1,104 @@
 // -----------------Interpolant of Complex Amplitudes ------------------------- //
 
-#include <ComplexAmpTest.h>
+#include <fstream>
+#include <iostream>
+#include <complex>
+#include <iomanip>
+#include <algorithm>
+#include "Interpolant.h"
 
 int main()
 {
 
-    ifstream inFile;
+//----------------------------
+//  read Almn data from file
+//----------------------------
+ifstream inFile;
 
-    inFile.open("A_l2_m2.dat");
-    if (!inFile)
-    {
-        cout << "Unable to open file \n";
-        exit(1); // terminate with error
-    }
+// initialize variables outside loops (speeds up by avoiding extra initialization cost)
+string line, temp;
+double y_col, e_col, test;
+int iter, input, num_cols;
+vector<double> ys, es; //y = p-2*e
+complex<double> Almn; 
 
-    // ------------------ gather important parameters --------------------------------- //
+int col_count = 2; // This will be the total number of columns
+inFile.open("GSF_model_data/A_l2_m2.dat"); 
+if (!inFile)
+	{
+    cout << "Unable to open file \n";
+    exit(1); // terminate with error
+	}
+else
+	{ // count the number of columns and advance past 1st row
+	  // (don't need to erase 1st row later)
+	getline(inFile,line);
+	istringstream ss(line);
+	ss >> temp;
+	ss >> temp;
+	while(ss >> test) col_count++;
+	}
+	
+// initialize 2 vectors (real and imaginary) for each n
+// used an array of vectors (instead of a vector of vectors) because we know the number of columns at this point
+vector<double> N_re[col_count-2], N_im[col_count-2]; // 1st index is each n, 2nd index is each orbit
 
-    int col_count = 83; // This is the total number of columns
+// loop through each orbit (rows in data file)
+while(getline(inFile,line))
+	{
+	istringstream ss(line);
 
-    //----------------- read file and create e and x column  -------------------------- //
 
-    vector<double> x, e; // x will be the value of p-2e
-    string line;
+	// I am using y instead of x to match the variable names in the existing code
+	ss >> y_col;    // extracts 1st col, which is y = p-2e
+	ss >> e_col;    // extracts 2nd col, which is e
 
-    while (getline(inFile, line)) // going to eventually have too throw away first line of output for each (title line)
-    {
-        istringstream ss(line);
-
-        double x_col, e_col;
-
-        ss >> x_col; // extracts 1st col, which is the p-2e column
-        ss >> e_col; // extracts 2nd col, which is the e column
-
-        // output the x-values and e-values to check:
-        // cout << x_col << " " << e_col << endl; Print e's and x's to verify
-
-        x.push_back(x_col);
-        e.push_back(e_col);
-    }
-    // end of use of the file stream
-    inFile.close();
-
-    vector<vector<double>> N_re, N_im; // This needs to stay outside the loop as to not erase it everytime
-
-    for (int num_cols = 1; num_cols < col_count; num_cols++)
-    {
-        cin.clear(); // This line is SUPER IMPORTANT: This refreshes the flags on input stream allowing the reopening of A_l2_m2.dat
-
-        //----------------- n column loop ( needs to loop over all n)  -------------------------- //
-
-        inFile.open("A_l2_m2.dat"); // This goes back to the directory which contains the file
-        if (!inFile)
-        {
-            cout << "Unable to open file \n";
-            exit(1); // terminate with error
-        }
-
-        vector<double> n_re, n_im;
-
-        int input;
-        while (getline(inFile, line))
-        {
-            istringstream ss(line);
-
-            complex<double> n_col;
-
-            ss >> n_col; // extracts 1st col, which is the p-2e column
-            ss >> n_col; // extracts 2nd col, which is the e column
-
-            int iter = 0;
-
-            // This while loop allows the loading of the columns from the first to the col_count-th column
-            while (iter < num_cols)
-            {
-                ss >> n_col;
-                iter++;
-            }
-
-            // output the n-values to check:
-            // cout << n_col << endl; // verify n's
-
-            n_re.push_back(n_col.real());
-            n_im.push_back(n_col.imag());
-        }
-
-        N_re.push_back(n_re);
-        N_im.push_back(n_im);
-
-        inFile.close();
-    }
-
-    // --------------------------------------------------------
-
-    // Get rid of the zeros in the beginning:
-
-    x.erase(x.begin());
-    e.erase(e.begin());
-
-    for (int i = 0; i < N_re.size(); i++)
-    {
-        N_re[i].erase(N_re[i].begin());
-        N_im[i].erase(N_im[i].begin()); // N_re and N_im are the same size
-    }
-
-    // --------------------------------------------------------
-
-    // Up to now, the last displayed value is the final real value in the final column of the .dat file. This means it worked!
-    // We have now read all the data, one column at a time, into the program, only leaving the interpolation of x,e and each column.
+	ys.push_back(y_col);
+	es.push_back(e_col);
+	
+	for(iter=0; iter<col_count-2; iter++) // loop through each n (columns in data file)
+		{
+		ss >> Almn;
+		// no need for another vector (n_re & n_im)
+		// can access the components of N_re and N_imag directly
+		N_re[iter].push_back(Almn.real());
+		N_im[iter].push_back(Almn.imag());
+		}
+	}
+inFile.close();  
+// We have now read all the data, one row at a time, into the program, only leaving the interpolation 
 
 
     // --------------------- Interpolation ------------------------------------- //
 
     // Generate the data used for interpolation:
 
-    vector<double> unique_x, unique_e;
+    vector<double> unique_y, unique_e;
 
-    unique_x.push_back(x[0]);
-    unique_e.push_back(e[0]);
+    unique_y.push_back(ys[0]);
+    unique_e.push_back(es[0]);
 
     // Find the unique x values:
-    for (int i = 1; i < x.size(); i++)
+    for (int i = 1; i < ys.size(); i++)
     {
-        if (x[i] == x[i - 1])
+        if (ys[i] == ys[i - 1])
         {
             continue;
         }
         else
         {
-            unique_x.push_back(x[i]);
+            unique_y.push_back(ys[i]);
         }
     }
 
     // Find the unique e values:
     for (int i = 1; i < col_count - 2; i++) // col_count -2 instead of -1 fixes the 0.402 extra line error.
     {
-        unique_e.push_back(e[i]);
+        unique_e.push_back(es[i]);
     }
 
-    vector<double> x_test_pre, e_test_pre;
-    double test_iter_x, test_iter_e;
+    vector<double> y_test_pre, e_test_pre;
+    double test_iter_y, test_iter_e;
 
     // Find midpoints of given data:
 
@@ -147,14 +107,14 @@ int main()
     double tot = (26.05-6.05)/0.25; 
     double tot_e = (0.802-0.002)/0.01;
     int divisions = 4;
-    double inc_x = 0.25/divisions;// the 0.25 increment would be the same as the original x data
+    double inc_y = 0.25/divisions;// the 0.25 increment would be the same as the original x data
     double inc_e = 0.01/divisions;// the 0.01 increment would be the same as the original e data
 
     for(int i = 0; i < (4*tot)+1;i++)
     {
         if ((i & divisions-1) != 0) // This avoids rewriting the original values, which were already used in training
         {
-        x_test_pre.push_back(i*inc_x + unique_x[0]);
+        y_test_pre.push_back(i*inc_y + unique_y[0]);
         e_test_pre.push_back(i*inc_e + unique_e[0]);
         } 
         
@@ -168,20 +128,20 @@ int main()
 // unique_x and unique_e contain the original unique values, so thay can be used with find to remove them from x_test_pre,e_test_pre
 
 // Test vector for finer sampling:
-    for (int i = 0; i < x_test_pre.size(); i++)
+    for (int i = 0; i < y_test_pre.size(); i++)
     {
-        cout << "x test pre values: " << x_test_pre[i] << endl; // Index goes from 0 to 80 (captures the data for all 81 n's)
+        cout << "x test pre values: " << y_test_pre[i] << endl; // Index goes from 0 to 80 (captures the data for all 81 n's)
     }
 
     // From here we can use a double for loop to generate the values of x and e as new columns: x_test and e_test
 
-    vector<double> x_test, e_test;
+    vector<double> y_test, e_test;
 
-    for (int i = 0; i < x_test_pre.size() - 1; i++)
+    for (int i = 0; i < y_test_pre.size() - 1; i++)
     {
         for (int j = 0; j < e_test_pre.size() - 1; j++)
         {
-            x_test.push_back(x_test_pre[i]);
+            y_test.push_back(y_test_pre[i]);
             e_test.push_back(e_test_pre[j]);
         }
     }
@@ -199,16 +159,16 @@ int main()
     for (int i = 0; i < col_count - 2; i++)
     {
         // Interpolant part:
-        N_Interp_re.push_back(new Interpolant(x, e, N_re[i]));
-        N_Interp_im.push_back(new Interpolant(x, e, N_im[i]));
+        N_Interp_re.push_back(new Interpolant(ys, es, N_re[i]));
+        N_Interp_im.push_back(new Interpolant(ys, es, N_im[i]));
 
         // Interpolated data part:
         vector<double> a_re, a_im;
 
-        for (int k = 0; k < x_test.size(); k++)
+        for (int k = 0; k < y_test.size(); k++)
         {
-            a_re.push_back(N_Interp_re[i]->eval(x_test[k], e_test[k]));
-            a_im.push_back(N_Interp_im[i]->eval(x_test[k], e_test[k]));
+            a_re.push_back(N_Interp_re[i]->eval(y_test[k], e_test[k]));
+            a_im.push_back(N_Interp_im[i]->eval(y_test[k], e_test[k]));
         }
 
         A_re.push_back(a_re);
@@ -235,10 +195,10 @@ int main()
     output << endl;
 
     // Loop over test rows
-    for (int i = 0; i < x_test.size(); i++)
+    for (int i = 0; i < y_test.size(); i++)
     {
         // Add the test values to the beginning of the row.
-        output << x_test[i] << " " << e_test[i];
+        output << y_test[i] << " " << e_test[i];
 
         // Loop through the columns and add each harmonic mode.
         for (int j = 0; j < A_re.size(); j++)
